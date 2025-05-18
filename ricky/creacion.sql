23fccd9cb6d84498652239b1cbfcf48583190e42
Ricardo Charahua Sánchez <ricardo.charahua@ucsm.edu.pe>
2025-05-18 11:01:41 -0500

    base de datos

---
+++ ricky/creacion.sql
@@ -0,0 +1,42 @@
+-- Crear la base de datos
+CREATE DATABASE inventario;
+
+-- Conectarse a la base de datos
+\c inventario;
+
+-- Crear tablas
+
+CREATE TABLE public.categorias (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) UNIQUE NOT NULL,
+    descripcion TEXT
+);
+
+CREATE TABLE public.usuarios (
+    id CHAR(8) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(150) UNIQUE NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
+    rol VARCHAR(50) DEFAULT 'usuario',
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE public.productos (
+    codigo SERIAL PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL,
+    descripcion TEXT,
+    precio DOUBLE PRECISION NOT NULL,
+    stock INTEGER NOT NULL CHECK (stock >= 0),
+    categoria_id INTEGER NOT NULL REFERENCES categorias(id),
+    stock_minimo INTEGER DEFAULT 5,
+    estado BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE public.movimientos_stock (
+    id CHAR(5) PRIMARY KEY,
+    codigo_producto INTEGER REFERENCES productos(codigo),
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cantidad INTEGER NOT NULL,
+    motivo TEXT,
+    usuario_id CHAR(8) REFERENCES usuarios(id)
+);
